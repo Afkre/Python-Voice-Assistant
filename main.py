a0d74c49595286d85830029d2cbd6db8c134dc77
Afkre <ahmetefkere@gmail.com>
2023-06-02 00:08:33 +0200

    2306020000

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import speech_recognition as spr
 
 r=spr.Recognizer()
+
+with spr.Microphone() as source:
+    audio  = r.listen(source)
+    voice    = r.recognize_google(audio , language='tr-TR')
+    print(voice)
