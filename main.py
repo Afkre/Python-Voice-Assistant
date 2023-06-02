5ae8871305bd02c0bc3f9b8592faf0473b2493d7
Afkre <ahmetefkere@gmail.com>
2023-06-02 10:21:21 +0200

    2306021000

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ from datetime import datetime
 import webbrowser
 import time
 from gtts import gTTS
-#from playsound import playsound
+from playsound import playsound
 import random
 import os
-from pydub import AudioSegment
-from pydub.playback import play
-
 
 r=spr.Recognizer()
 
@@ -31,34 +28,23 @@ def response(voice):
         speak('Mir geht es gut, und wie geht es Ihnen?')
     if 'Wie viel Uhr ist es?':
         speak(datetime.now().strftime('5H:5M:%S'))
-    if   'such im Internet' in voice:
+    if  'such im Internet' in voice:
         search = record ('Was wollen Sie im Internet suchen?') 
-        url =  'http://google.com/search?q='+search
+        url =  'https://google.com/search?q='+search
         webbrowser.get().open(url)
         speak(search + 'gefunden')
     if 'okey' in voice:
         speak('Wenn Sie weitere Fragen haben, stehe ich Ihnen gerne zur Verfügung.')
         exit()
 
-"""
 def speak(string):
-    antwort = gTTS(string, lang='de')
+    tts = gTTS(string, lang='de')
     rand = random.randint(1,5000)
     file = 'audio-'+str(rand)+'.mp3'
-    antwort.save(file)
+    tts.save(file)
     playsound(file)
     os.remove(file)
-"""
-def speak(string):
-    antwort = gTTS(string, lang='de')
-    rand = random.randint(1, 5000)
-    file = 'audio-' + str(rand) + '.mp3'
-    antwort.save(file)
-
-    audio = AudioSegment.from_mp3(file)
-    play(audio)
-
-    os.remove(file)    
+    
 
 speak('Wie kann ich Ihnen helfen?')
 time.sleep(1)
